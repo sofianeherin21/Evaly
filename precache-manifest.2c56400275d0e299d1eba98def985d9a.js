977ee2a603c5928b2937f76e0e5175d67a870ee7
Sofia Neherin <sofia.neherin@gmail.com>
2020-08-12 22:06:30 +0600

    Updates

---
+++ precache-manifest.2c56400275d0e299d1eba98def985d9a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bd9889b397420897da64f4299029c0",
+    "url": "/Evaly/index.html"
+  },
+  {
+    "revision": "c3a3dd1ff388e96e1f32",
+    "url": "/Evaly/static/css/2.c374b366.chunk.css"
+  },
+  {
+    "revision": "9a3d27cae3c1b92d5d91",
+    "url": "/Evaly/static/css/main.dd94e5cc.chunk.css"
+  },
+  {
+    "revision": "c3a3dd1ff388e96e1f32",
+    "url": "/Evaly/static/js/2.704094fe.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Evaly/static/js/2.704094fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a3d27cae3c1b92d5d91",
+    "url": "/Evaly/static/js/main.c4a935c6.chunk.js"
+  },
+  {
+    "revision": "c00f3b49474f18e0cf8a",
+    "url": "/Evaly/static/js/runtime-main.33ee8b4e.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Evaly/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Evaly/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Evaly/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Evaly/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Evaly/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0d571a00b1a0234f4133a14933af4909.js"
+  "/Evaly/precache-manifest.2c56400275d0e299d1eba98def985d9a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Evaly/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
