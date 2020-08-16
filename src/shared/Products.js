ede5316695b0728b95e3604685a2abb48f4b0d88
Sofia Neherin <sofia.neherin@gmail.com>
2020-08-16 18:09:30 +0600

    path change

--- src/shared/Products.js
+++ src/shared/Products.js
@@ -7,56 +7,56 @@ export const PRODUCTS=[
 		{
 			id:0,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag0.png',
+			img:'assets/pro/bag0.png',
 			url:'',
 			price: 1480.00
 		},
 		{
 			id:1,
 			name:'President Fashionable Laptop Bag for Men - Black ( 5350)',
-			img:'/assets/pro/bag1.png',
+			img:'assets/pro/bag1.png',
 			url:'',
 			price: 2650.00
 		},
 		{
 			id:2,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag2.png',
+			img:'assets/pro/bag2.png',
 			url:'',
 			price: 4580.00
 		},
 		{
 			id:3,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag3.png',
+			img:'assets/pro/bag3.png',
 			url:'',
 			price: 3080.00
 		},
 		{
 			id:4,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag4.png',
+			img:'assets/pro/bag4.png',
 			url:'',
 			price: 2650.00
 		},
 		{
 			id:5,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag5.png',
+			img:'assets/pro/bag5.png',
 			url:'',
 			price: 1480
 		},
 		{
 			id:6,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag6.png',
+			img:'assets/pro/bag6.png',
 			url:'',
 			price: 4800.00
 		},
 		{
 			id:7,
 			name:'President Nylon Laptop Backpack for Unisex',
-			img:'/assets/pro/bag7.png',
+			img:'assets/pro/bag7.png',
 			url:'',
 			price: 850.00
 		}]
@@ -69,56 +69,56 @@ export const PRODUCTS=[
 		{
 			id:0,
 			name:'Eyeshadow Pencil - Emerald - (ESP008)',
-			img:'/assets/pro/beauty0.png',
+			img:'assets/pro/beauty0.png',
 			url:'',
 			price: 750.00
 		},
 		{
 			id:1,
 			name:'Eyeshadow Pencil - Salmon - (ESP003)',
-			img:'/assets/pro/beauty1.png',
+			img:'assets/pro/beauty1.png',
 			url:'',
 			price: 750.00
 		},
 		{
 			id:2,
 			name:'Eyeshadow - 105 - (PE005)',
-			img:'/assets/pro/beauty2.png',
+			img:'assets/pro/beauty2.png',
 			url:'',
 			price: 1100.00
 		},
 		{
 			id:3,
 			name:'Note Long Lash Mascara - Black',
-			img:'/assets/pro/beauty3.png',
+			img:'assets/pro/beauty3.png',
 			url:'',
 			price: 700.00
 		},
 		{
 			id:4,
 			name:'Ultra Black Dipliner - Black - (UBD001)',
-			img:'/assets/pro/beauty4.png',
+			img:'assets/pro/beauty4.png',
 			url:'',
 			price: 500.00
 		},
 		{
 			id:5,
 			name:'Focallure 12psc professional pencil Soft Matte Lipstick',
-			img:'/assets/pro/beauty5.png',
+			img:'assets/pro/beauty5.png',
 			url:'',
 			price: 580.00
 		},
 		{
 			id:6,
 			name:'Rimmel Stay Lip contouring Pencil Matte Lipstick',
-			img:'/assets/pro/beauty6.png',
+			img:'assets/pro/beauty6.png',
 			url:'',
 			price: 640.00
 		},
 		{
 			id:7,
 			name:'Note Rich Color Lipstick-Topaz',
-			img:'/assets/pro/beauty7.png',
+			img:'assets/pro/beauty7.png',
 			url:'',
 			price: 495.00
 		}]
@@ -131,56 +131,56 @@ export const PRODUCTS=[
 		{
 			id:0,
 			name:'Mi Band 5 AMOLED screen Smart Fitness ',
-			img:'/assets/pro/watch0.png',
+			img:'assets/pro/watch0.png',
 			url:'',
 			price: 2392.00
 		},
 		{
 			id:1,
 			name:'W8 Plus IP67 Fitness Tracker Magnetic Metal',
-			img:'/assets/pro/watch1.png',
+			img:'assets/pro/watch1.png',
 			url:'',
 			price: 1999.00
 		},
 		{
 			id:2,
 			name:'W8 Plus IP67 Fitness Tracker Magnetic Metal ',
-			img:'/assets/pro/watch2.png',
+			img:'assets/pro/watch2.png',
 			url:'',
 			price: 1100.00
 		},
 		{
 			id:3,
 			name:'Leather Analog Wrist Watch For Men - Black & White',
-			img:'/assets/pro/watch3.png',
+			img:'assets/pro/watch3.png',
 			url:'',
 			price: 700.00
 		},
 		{
 			id:4,
 			name:'T55 Series 5 Smart Watch - Black',
-			img:'/assets/pro/watch4.png',
+			img:'assets/pro/watch4.png',
 			url:'',
 			price: 4290.00
 		},
 		{
 			id:5,
 			name:'W26 HD Screen Smart Watch',
-			img:'/assets/pro/watch5.png',
+			img:'assets/pro/watch5.png',
 			url:'',
 			price: 2580.00
 		},
 		{
 			id:6,
 			name:'T55 Series 5 Smart Watch - Black',
-			img:'/assets/pro/watch6.png',
+			img:'assets/pro/watch6.png',
 			url:'',
 			price: 4290.00
 		},
 		{
 			id:7,
 			name:'Colmi P8 Pro Smart Watch - Sky Blue',
-			img:'/assets/pro/watch7.png',
+			img:'assets/pro/watch7.png',
 			url:'',
 			price: 1900.00
 		}]
@@ -193,56 +193,56 @@ export const PRODUCTS=[
 		{
 			id:0,
 			name:'Eyeshadow Pencil - Emerald - (ESP008)',
-			img:'/assets/pro/glass0.png',
+			img:'assets/pro/glass0.png',
 			url:'',
 			price: 750.00
 		},
 		{
 			id:1,
 			name:'Eyeshadow Pencil - Salmon - (ESP003)',
-			img:'/assets/pro/glass1.png',
+			img:'assets/pro/glass1.png',
 			url:'',
 			price: 750.00
 		},
 		{
 			id:2,
 			name:'Eyeshadow - 105 - (PE005)',
-			img:'/assets/pro/glass2.png',
+			img:'assets/pro/glass2.png',
 			url:'',
 			price: 1100.00
 		},
 		{
 			id:3,
 			name:'Note Long Lash Mascara - Black',
-			img:'/assets/pro/glass3.png',
+			img:'assets/pro/glass3.png',
 			url:'',
 			price: 700.00
 		},
 		{
 			id:4,
 			name:'Ultra Black Dipliner - Black - (UBD001)',
-			img:'/assets/pro/glass4.png',
+			img:'assets/pro/glass4.png',
 			url:'',
 			price: 500.00
 		},
 		{
 			id:5,
 			name:'Focallure 12psc professional pencil Soft Matte Lipstick',
-			img:'/assets/pro/glass5.png',
+			img:'assets/pro/glass5.png',
 			url:'',
 			price: 580.00
 		},
 		{
 			id:6,
 			name:'Rimmel Stay Lip contouring Pencil Matte Lipstick',
-			img:'/assets/pro/glass6.png',
+			img:'assets/pro/glass6.png',
 			url:'',
 			price: 640.00
 		},
 		{
 			id:7,
 			name:'Note Rich Color Lipstick-Topaz',
-			img:'/assets/pro/glass7.png',
+			img:'assets/pro/glass7.png',
 			url:'',
 			price: 495.00
 		}]
